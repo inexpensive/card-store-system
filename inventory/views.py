71256c77fe386e01701ab663efde76e28de85cf4
inexpensive <lawrence.bouzane@gmail.com>
2017-04-30 02:03:58 -0230

    changed the index to show all sets, and have each set navigate to showing its cards :)

--- inventory/views.py
+++ inventory/views.py
@@ -1,23 +1,23 @@
 from django.core.urlresolvers import reverse
 from django.template import loader
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.utils import timezone
 from django.views import generic
-from .models import Card
+from .models import Card, Set
 
 
 class IndexView(generic.ListView):
     template_name = 'inventory/index.html'
-    context_object_name = 'card_list'
+    context_object_name = 'set_list'
 
     def get_queryset(self):
-        return Card.objects.order_by('multiverse_id', 'foil')
+        return Set.objects.order_by('-release_date')
 
 
-class DetailView(generic.DetailView):
+class SetView(generic.DetailView):
     model = Card
-    template_name = 'inventory/detail.html'
+    template_name = 'inventory/set.html'
 
     def get_queryset(self):
         """
@@ -32,17 +32,17 @@ class ResultsView(generic.DetailView):
 
 
 def index(request):
-    card_list = Card.objects.filter(foil=False).order_by('name')
+    set_list = Set.objects.order_by('-release_date')
     template = loader.get_template('inventory/index.html')
     context = {
-        'card_list' : card_list,
+        'set_list' : set_list,
     }
     return render(request, 'inventory/index.html', context)
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Card, pk=question_id)
-    return render(request, 'inventory/detail.html', {'question': question})
+def cardset(request, set_id):
+    card_list = get_list_or_404(Card.objects.order_by('multiverse_id'), set=set_id, foil=False, condition='NM')
+    return render(request, 'inventory/set.html', {'card_list': card_list})
 
 
 def results(request, question_id):
